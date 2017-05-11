f30e349af6e9d3998b77a3ad9536ffaaf79960d5
stdio2016 <randomfrank8@gmail.com>
2017-05-11 11:03:26 +0800

    remove unused variables in CFG

--- js/cfg.js
+++ js/cfg.js
@@ -23,19 +23,37 @@ CFG.prototype.buildGraph = function () {
 };
 
 CFG.prototype.removeUnused = function () {
-  var used = new Set();
+  var used = []; // set of variables
+  for (var i = 0; i < this.variables.length; i++) {
+    this.variables[i].mark = false;
+  }
   for (var i = 0; i < this.productions.length; i++) {
     var p = this.productions[i];
-    var allTerms = true;
-    for (var j = 0; j < p.toVars.length; j++) {
-      if (typeof p.to[j] !== "string") {
-        allTerms = false; break;
+    if (p.toVars.length == 0 && !p.from.mark) {
+      used.push(p.from);
+      p.from.mark = true;
+    }
+    p.markCount = 0;
+  }
+  for (var i = 0; i < used.length; i++) {
+    for (var j = 0; j < used[i].inProductions.length; j++) {
+      var p = used[i].inProductions[j];
+      p.markCount++;
+      if (p.toVars.length == p.markCount && !p.from.mark) {
+        used.push(p.from);
+        p.from.mark = true;
       }
     }
-    if (allTerms) {
-      used.add(p.from);
+  }
+  var newP = [];
+  for (var i = 0; i < this.productions.length; i++) {
+    var p = this.productions[i];
+    if (p.toVars.length == p.markCount) {
+      newP.push(p);
     }
   }
+  this.variables = used;
+  this.productions = newP;
 };
 
 function CFGVariable(name) {
@@ -43,6 +61,7 @@ function CFGVariable(name) {
   this.id = 0;
   this.inProductions = [];
   this.productions = [];
+  this.mark = false;
 }
 
 function CFGProduction(from, to) {
@@ -50,10 +69,11 @@ function CFGProduction(from, to) {
   this.to = to;
   this.toVars = [];
   for (var i = 0; i < to.length; i++) {
-    if (to instanceof CFGVariable && this.toVars.indexOf(to) == -1) {
-      this.toVars.push(to);
+    if (to[i] instanceof CFGVariable && this.toVars.indexOf(to[i]) == -1) {
+      this.toVars.push(to[i]);
     }
   }
+  this.markCount = 0;
 }
 
 CFGProduction.prototype.toString = function () {
